56a4384c1125cf56906be1a42b9b09dcfb042efa
Tunelko <tunelko@gmail.com>
2015-03-09 19:10:04 +0100

    Create decompiled_exe.cs

---
+++ decompiled_exe.cs
@@ -0,0 +1,58 @@
+using System;
+
+namespace Program
+{
+	class MainClass
+	{
+
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Decrypt: ");
+			string input = "IH4+HpDgiVwQj2rBRxZyLVgVTiQFxFjXMqcC4U8kPrF4zm9I3NBtaol9hChF1Hvt3+bEqW5tujSPHW6XO50tsALNmrgjO0taLpZ4FOrrOJPI1i6VP747B4ixN5/AzLFlNSno+wHiB2dDPgrBw29mXH8pqwvYK7k4/3ylpB4NW+p012s8qCt7eqeEQFURgQz53qoO5TCi2VaVzFJkOXpNxCEgKZMIpkWn/yXQrr5mPpDGZpwkBxicd1uQlrNaRZoJJVIz5/pVTXEk7HzO4KWp2kwvJqhdjMWN2m4OyIBWO3QQaIr5Pd5e6DjC26UGdnpzMGBs3mMKEOzrMFrFVNHhNxn3RdLWdCH80KwC0ObaMYn5XrGMWnGrHLTzxqN3kZYtkLED8CxoyWr3jb9Ifer/2ZsW3k9KMbLwlm6mRaWSab3qBI/f07czw1IwoEJ92OrjceLAP8u+NN3bHQSu8u/2qI6hFw8n/vEN29YRB9PhlrKAKpEZv93z9Cb4zf5o73NA1RTi+xZc3UG1mOTNtFeqG7AcyYyMpc/yNJSeaotgNzMgKHFQUQFOBwba3qd5hDjTyW4leoKj38D5Oe/DB4NLBI0cdCqu4RLl0tse5X3BXo0GrXkDxvIhLAZ55GX6FmlfqmCwv1FJcGfK0QqHkMxIUeEuA0N8mrc5bA1q/bh8UxKEzAnAuGlG+jx4w3+Hkv9XFWLEyYX6eHvIJ1DI0mB7TViAX9Xg8WO7qdDkMBbdJnCQsQPwLGjJahSPE1U6BW6BrOdOBDIYuVzPsN8EyGI/dZsW3k9KMbLw/TSHZaGqjNMDkF97OpQbj3DW6FSqTR0F40JJJ92qhQXAMcMyiYMQW2A81d5xCgyDIPlcpgp6ee0H9H8E11aplmMThQHmAFgYecEOJAFAU9Q0qW9e3+KWs192fgl/kpMRtD6HODh7tNRkzph7aBiVldp6GIs/peNePwx6pbCmr/ERInU3GcoxgC1no/siXs2dm0C8A41auBIdH0UGbDVEbMOVlY8+igPW";
+			string enc = Encryption.Decrypt(input, "N2NhNWRkNzc5MGUwNWFkYQ==");
+			Console.WriteLine(enc);
+
+		}
+	}
+}
+
+
+Decrypt: 
+shh many obscure
+trained
+
+very slapazzle is 1
+very razzel is 0
+
+very things is [['a','e564bdcf'],['doge','3830b8bf'],['knows','b68b3ea8'],['more','6ee9565e'],['about','057d5f1a'],['security','26a5f095'],['than','eb29b574'],['you','3a71cd83']]
+
+
+very unwow is false
+
+such shout much thang
+    rly thang is 'noise'
+    	much very on as 0 next on smaller 8 next on more 1
+        	plz dazzle with slapazzle on
+        wow
+    but
+		much very on as 0 next on smaller 8 next on more 1
+        	plz dazzle with razzel on
+        wow
+    wow
+wow
+
+such what much isit
+    rly isit
+    	plz shout with 'noise'
+    but
+    	plz shout with 'quiet'
+    wow
+wow
+
+such dazzle much razzle splazzle
+	plz console.loge with things[splazzle][razzle]
+wow
+
+
+shh loud
+plz what with unwow
